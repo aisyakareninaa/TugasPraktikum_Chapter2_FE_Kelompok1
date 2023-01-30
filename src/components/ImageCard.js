f40e4035ded6f2262cdfb90a434269dd068bdfd7
Ice <100953845+aisyakareninaa@users.noreply.github.com>
2023-01-30 19:36:51 +0700

    merubah API

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,41 +1,24 @@
 import React from "react";
 
 const ImageCard = ({ image }) => {
-  const tags = image.tags.split(",");
 
   return (
-    <div className="bg-white max-w-sm rounded overflow-hidden shadow-lg ml-14">
-      <img src={image.webformatURL} alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-indigo-500 text-xl mb-2">
-          Photo by {image.user}
+    <div className="bg-white rounded-md overflow-hidden">
+      <img src={image.image} alt="" className="w-full h-36 md:h-48 object-contain p-2 my-5"/>
+      <div className="px-6 py-8">
+        <div className="font-bold text-indigo-400 text-lg">
+          {image.name}
         </div>
         <div className="text-indigo-400">
           <ul>
             <li>
-              <strong>Views: </strong>
-              {image.views}
+              {image.description}
             </li>
-            <li>
-              <strong>Downloads: </strong>
-              {image.downloads}
-            </li>
-            <li>
-              <strong>Likes: </strong>
-              {image.likes}
+            <li className="text-md">
+              <strong>{image.price}</strong>
             </li>
           </ul>
         </div>
-      </div>
-      <div className="px-6 py-4">
-        {tags.map((tag, index) => (
-          <span
-            key={index}
-            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-          >
-            #{tag}
-          </span>
-        ))}
       </div>
     </div>
   );
