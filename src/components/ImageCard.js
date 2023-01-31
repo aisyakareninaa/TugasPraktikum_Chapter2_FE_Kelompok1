808ba334fe41508782c249b077b3e0875f8d33a7
Ice <100953845+aisyakareninaa@users.noreply.github.com>
2023-01-31 17:07:03 +0700

    Update ImageCard.js

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,30 +1,26 @@
 import React from "react";
 
 const ImageCard = ({ image }) => {
-  const desc = image.description;
-  const res = desc.substr(0, 40);
+
   return (
-    <a href="" className="bg-white rounded-lg overflow-hidden mx-auto w-64 space-x-0">
-      <img
-        src={image.image}
-        alt=""
-        className="w-full h-48 md:h-48 object-contain p-2 my-5 rounded-md"
-      />
+    <div className="bg-white rounded-md overflow-hidden">
+      <img src={image.image} alt="" className="w-full h-36 md:h-48 object-contain p-2 my-5"/>
       <div className="px-6 py-8">
-        <div className="font-bold text-indigo-400 text-lg mb-3">{image.name}</div>
+        <div className="font-bold text-indigo-400 text-lg">
+          {image.name}
+        </div>
         <div className="text-indigo-400">
           <ul>
-            <li>{res}...</li>
-            <li className="text-base">
-              <strong>{image.price}</strong>
+            <li>
+              {image.description}
             </li>
-            <li className="text-indigo-300 text-sm">
-              {image.category}
+            <li className="text-md">
+              <strong>Rp.{image.price},-</strong>
             </li>
           </ul>
         </div>
       </div>
-    </a>
+    </div>
   );
 };
 
