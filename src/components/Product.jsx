371a2cfa33e0165f059b6ed5ba98ae523da09a32
Ice <100953845+aisyakareninaa@users.noreply.github.com>
2023-01-31 18:12:49 +0700

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,14 +3,12 @@ import data from "../components/data.json";
 import ImageCard from "./ImageCard";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
   const [search, setSearch] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
   return (
         <div className="container mx-auto mb-16">
       <div className="text-center font-bold">
-        <h1 className="text-4xl text-indigo-600">Get your</h1>
-        <h2 className="text-4xl text-indigo-600 mb-8 mt-4">hapiness now!</h2>
+        <h1 className="text-4xl text-indigo-600">Shop your</h1>
+        <h2 className="text-4xl text-indigo-600 mb-8 mt-4">product now!</h2>
       </div>
       <div className="max-w-sm rounded overflow-hidden my-10 mx-auto mt-0">
       <form className="w-full max-w-sm bg-white/60 p-1">
@@ -24,24 +22,18 @@ function App() {
         </div>
       </form>
     </div>
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        
           {
-            data 
-              .filter((val) => {
-                if(searchTerm == ""){
-                  return val;
-                }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
-                  return val;
-                }
-              })
-              .map((val) => {
-                return(
-                  <ImageCard key={val.id} image={val} coba={val.image} />
-                )
-              })
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+            {data.filter((image) => {
+              return search.toLowerCase() === '' ? image : image.name.toLowerCase().includes(search)
+            }).map((image) => (
+              <ImageCard key={image.id} image={image} coba={image.image} />
+            ))}
+          </div>
           }
         </div>
-      </div>
+    
   )
 }
 
