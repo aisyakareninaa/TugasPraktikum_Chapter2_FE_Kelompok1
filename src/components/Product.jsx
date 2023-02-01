4d395504a7f603a6c1f7f064c9d58fbaf40ff9f1
Ice <100953845+aisyakareninaa@users.noreply.github.com>
2023-02-01 10:42:49 +0700

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,41 +1,72 @@
-import React, { Fragment, useState } from "react";
-import data from "../components/data.json";
-import ImageCard from "./ImageCard";
+import React, { Fragment, useEffect, useState } from 'react';
+import data from '../components/data.json';
+import ImageCard from './ImageCard';
 
 function App() {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
   const [isLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [q, setQ] = useState("");
-  const [searchParam] = useState(["name", "description", "category"]);
-  const [filterParam, setFilterParam] = useState(["All"]);
+  const [q, setQ] = useState('');
+  const [searchParam] = useState(['name', 'description', 'category']);
+  const [labelCategory] = useState([
+    { value: 'All', label: 'All (Filter By Category)' },
+    { value: 'Ready', label: 'Ready' },
+    { value: 'Barang Bekas', label: 'Barang Bekas' },
+    { value: 'Pre-Order', label: 'Pre-Order' },
+  ]);
+  const [filterParam, setFilterParam] = useState('All');
+  const [product, setProduct] = useState([]);
 
   function cari(data) {
-    isLoading(true)
+    isLoading(true);
     return data.filter((item) => {
-        if (data.category == filterParam) {
-            return searchParam.some((newItem) => {
-                return (
-                    item[newItem]
-                        .toString()
-                        .toLowerCase()
-                        .indexOf(q.toLowerCase()) > -1
-                );
-            });
-        } else if (filterParam == "All") {
-            return searchParam.some((newItem) => {
-                return (
-                    item[newItem]
-                        .toString()
-                        .toLowerCase()
-                        .indexOf(q.toLowerCase()) > -1
-                );
-            });
-        }
-        isLoading(false);
+      if (data.category == filterParam) {
+        return searchParam.some((newItem) => {
+          return item[newItem].toString().toLowerCase().indexOf(q.toLowerCase()) > -1;
+        });
+      } else if (filterParam == 'All') {
+        return searchParam.some((newItem) => {
+          return item[newItem].toString().toLowerCase().indexOf(q.toLowerCase()) > -1;
+        });
+      }
+      isLoading(false);
     });
   }
-  
+
+  useEffect(() => {
+    console.log(filterParam);
+    const searchTerm = search.toLowerCase();
+    let filteredData;
+    if (search === '' && filterParam === 'All') {
+      setProduct(data);
+    } else {
+      if (search !== '' && filterParam === 'All') {
+        filteredData = data.filter((value) => {
+          return value.name.toLowerCase().match(new RegExp(searchTerm, 'g'));
+        });
+      } else if (search !== '' && filterParam !== 'All') {
+        filteredData = data.filter((value) => {
+          return (
+            value.name.toLowerCase().match(new RegExp(searchTerm, 'g')) &&
+            value.category === filterParam
+          );
+        });
+      } else if (search === '' && filterParam === 'All') {
+        filteredData = data.filter((value) => {
+          return value.category === filterParam;
+        });
+      } else if (search === '' && filterParam !== 'All') {
+        filteredData = data.filter((value) => {
+          return value.category === filterParam;
+        });
+      }
+      setProduct(filteredData);
+    }
+  }, [filterParam, search]);
+
+  useEffect(() => {
+    setProduct(data);
+  }, []);
 
   return (
     <div className="container mx-auto mb-16">
@@ -55,45 +86,35 @@ function App() {
           </div>
         </form>
         <div className="place-items-center flex-2">
-        <select
-          onChange={(e) => {
-            setFilterParam(e.target.value);
-          }}
-          className="custom-select px-4 py-3 rounded-md bg-white/70 text-indigo-500"
-          aria-label="Filter Product By Category"
-        >
-          <option value="All">Filter By Category</option>
-          <option value="Ready">Ready</option>
-          <option value="Barang Bekas">Barang Bekas</option>
-          <option value="Pre-Order">Pre-Order</option>
-        </select>
-        <span className="focus"></span>
-      </div>
+          <select
+            onChange={(e) => {
+              setFilterParam(e.target.value);
+            }}
+            className="custom-select px-4 py-3 rounded-md bg-white/70 text-indigo-500"
+            aria-label="Filter Product By Category"
+          >
+            {labelCategory.map((item) => {
+              return <option value={item.value}>{item.label}</option>;
+            })}
+          </select>
+          <span className="focus"></span>
+        </div>
       </div>
       <div className="text-center text-indigo-500 text-lg mb-8">
-        <p>{data.length} Product found</p>
+        <p>{product.length} Product found</p>
       </div>
       {isLoading && <h1 className="text-5xl text-center mx-auto mt-32">Loading ....</h1>}
       {!isLoading && data.length === 0 && (
         <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
       )}
-      {
-        data.length !== 0 &&
-        <div
-          className="grid grid-cols-2 lg:grid-cols-4 gap-4"
-          onClick={() => setShowModal(true)}
-        >
-          {data
-            .filter((image) => {
-              return search.toLowerCase() === ""
-                ? image
-                : image.name.toLowerCase().includes(search);
-            })
+      {data.length !== 0 && (
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4" onClick={() => setShowModal(true)}>
+          {product
             .map((image) => (
               <ImageCard key={image.id} image={image} coba={image.image} />
             ))}
         </div>
-      }
+      )}
     </div>
   );
 }
