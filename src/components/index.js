ebb4f98ff275e46c20d156bf563a7153f9e53cd5
Ice <100953845+aisyakareninaa@users.noreply.github.com>
2023-02-03 15:28:39 +0700

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import Home from './Home'
 import Product from './Product';
+import Member from './Member'
 import Footer from './Footer'
 
 const index = () => {
@@ -10,6 +11,9 @@ const index = () => {
         <Navbar />
         <section id='home'>
           <Home />
+        </section>
+        <section id='member'>
+          <Member />
         </section>
         <section id='product'>
           <Product />
