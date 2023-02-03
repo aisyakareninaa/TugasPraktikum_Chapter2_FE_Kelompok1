3f6c85fb8ec9e8cbd8eab69fafccfed492d8b02f
Ice <100953845+aisyakareninaa@users.noreply.github.com>
2023-02-03 14:52:03 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Index from "./components/index";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Member from "./components/Member"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/navbar" element={<Navbar />} />
           <Route path="/" element={<Index />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/member" element={<Member />} />
           <Route path="/footer" element={<Footer />} />
         </Routes>
       </Router>
